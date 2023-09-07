2eb0c8d48e9e21d2cf1469caa5597a1575970695
Jose <rev.jose66@gmail.com>
2023-09-07 14:56:26 -0700

    first commit

--- src/main/java/Magic8Ball.java
+++ src/main/java/Magic8Ball.java
@@ -6,50 +6,59 @@ import java.util.Scanner;
  * To get a response from the user, use the getUserQuestion() method at the bottom of this file
  */
 public class Magic8Ball {
-
-    // 1. Make a main method that includes all the steps below
-
-    // 2. Ask the user to enter a question for the magic 8-ball
-
-
-    // 3. Call the getUserQuestion() method to get a question from the user
-    //    String userQuestion = getUserQuestion();
-
-
-    // 4. Make a variable and initialize it to a random number
-    //
-    //    // Get random a number from: [0, 1, 2, 3]
-    //    int randNumber = new Random().nextInt(4);
-
-
-    // 5. If the random number is 0
-
-        // Tell the user "Yes"
-
-
-    // 6. If the random number is 1
-
-        // Tell the user "No"
-
-
-    // 7. If the random number is 2
-
-        // Tell the user a custom response
-
-
-    // 8. If the random number is 3
-
-        // Tell the user a custom response
-
-
-    /**
-     * @return The user's response as a String
-     */
     public static String getUserQuestion(){
+
         Scanner scanner = new Scanner(System.in);
         String question = scanner.nextLine();
         return question;
     }
+
+
+    public static void main(String[] args){
+
+        Scanner keyboardInput = new Scanner(System.in);
+        // Get random a number from: [0, 1, 2, 3]
+        Random randomInt = new Random();
+
+
+        String question;
+        int randNumber = new Random().nextInt(4);
+        String answer;
+
+
+        System.out.print("Ask a yes or no question: ");
+        //  Ask the user to enter a question for the magic 8-ball
+
+        question = keyboardInput.nextLine();
+
+        if(randNumber == 0){
+            // Tell the user "Yes"
+            answer = "Yes";
+        } else if (randNumber == 1) {
+            // Tell the user "No"
+            answer = "No";
+        }else if (randNumber == 2) {
+            // Tell the user a custom response
+            answer = "Ask the question again";
+        } else if (randNumber == 3) {
+            // Tell the user a custom response
+            answer = "Maybe in the near future";
+        } else {
+            answer = "Ask again later";
+        }
+        System.out.println(answer);
+    }
+
+
+
+
+
+
+
+
+
+
+
 }
 
 
